b34960447934198093aaab78dee8f8ab66e7e56f
RaulCasado <raulcasado969@gmail.com>
2024-05-14 17:47:23 +0200

    feat:added minimum boxes

--- exercices.py
+++ exercices.py
@@ -865,3 +865,30 @@ def most_common_word(paragraph, banned):
         return most_common_word
 
 print(most_common_word("Bob hit a ball, the hit BALL flew far after it was hit.",["hit"]))
+
+'''
+You are given an array apple of size n and an array capacity of size m.
+
+There are n packs where the ith pack contains apple[i] apples. There are m boxes as well, and the ith box has a capacity of capacity[i] apples.
+
+Return the minimum number of boxes you need to select to redistribute these n packs of apples into boxes.
+
+Note that, apples from the same pack can be distributed into different boxes.
+'''
+def minimum_boxes(apple, capacity):
+    capacity.sort(reverse=True)
+    total_apples = sum(apple)
+    boxes_used = 0
+    apples_packed = 0
+
+    for cap in capacity:
+        if apples_packed >= total_apples:
+            break
+        
+        apples_in_box = min(cap, total_apples - apples_packed)
+        apples_packed += apples_in_box
+        boxes_used += 1
+
+    return boxes_used
+
+print(minimum_boxes(apple=[1, 3, 2], capacity=[4, 3, 1, 5, 2]))
